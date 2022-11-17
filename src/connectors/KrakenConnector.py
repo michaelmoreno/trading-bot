36b73f377591767c0308e228673fa8522d5941bc
Michael Moreno <michaelmoreno.dev@gmail.com>
2022-11-16 22:31:01 -0700

    :diamond_shape_with_a_dot_inside: `add` KrakenConnector REST auth .request()

--- src/connectors/KrakenConnector.py
+++ src/connectors/KrakenConnector.py
@@ -1,19 +1,35 @@
 from typing import List, Tuple
 import requests
+import base64
+import hashlib
+import hmac
+import urllib.parse
+import time
 
 class KrakenConnector:
     base_http: str = 'https://api.kraken.com'
     base_ws: str = 'wss://ws.kraken.com'
     _offered_pairs: List[Tuple[str, str]]
 
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, api_priv: str):
         self.api_key = api_key
+        self.api_priv = api_priv
         self._offered_pairs = []
 
-    @property 
-    def offered_pairs(self) -> List[Tuple[str, str]]:
-        return self._offered_pairs or self.get_offered_pairs()
+    def generate_signature(self, urlpath: str, payload: dict) -> str:
+        postdata = urllib.parse.urlencode(payload)
+        encoded = (payload['nonce'] + postdata).encode()
+        message = urlpath.encode() + hashlib.sha256(encoded).digest()
 
-    def get_offered_pairs(self) -> List[Tuple[str, str]]:
-        result = requests.get(f'{self.base_http}/0/public/AssetPairs').json()['result']
-        return [(pair['base'], pair['quote']) for pair in result.values()]
+        mac = hmac.new(base64.b64decode(self.api_priv), message, hashlib.sha512)
+        sigdigest = base64.b64encode(mac.digest())
+        return sigdigest.decode()
+
+    def request(self, uri_path: str, payload: dict) -> requests.models.Response:
+        payload['nonce'] = str(int(time.time() * 1000))
+        headers = {
+            'API-Key': self.api_key,
+            'API-Sign': self.generate_signature(uri_path, payload)
+        }
+        res = requests.post(self.base_http + uri_path, headers=headers, data=payload)
+        return res
--- tests/connectors/test_KrakenConnector.py
+++ tests/connectors/test_KrakenConnector.py
@@ -1,2 +1,11 @@
 from src.connectors.KrakenConnector import KrakenConnector
+from typing import Dict
+from dotenv import dotenv_values
 
+config: Dict[str,str] = dotenv_values(".env") # type: ignore
+
+def test_request():
+    connector = KrakenConnector(config['KRAKEN_API'], config['KRAKEN_PRIV'])
+    data = {}
+    res = connector.request('/0/private/Balance', data).json()
+    assert res['error'] == []
