2c5c05d4cc98b73cec05a6abd87da7e21b9c6f4e
Michael Moreno <michaelmoreno.dev@gmail.com>
2022-11-16 22:48:16 -0700

    :fleur_de_lis: `semantic` declare .request() on Connector

--- src/connectors/Connector.py
+++ src/connectors/Connector.py
@@ -1,5 +1,8 @@
 from typing import Protocol
+import requests
 
 class Connector(Protocol):
     offered_pairs: property
-    
+
+    def request(self, uri_path: str, payload: dict) -> requests.models.Response:
+        ...
